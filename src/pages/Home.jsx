48ab726a231568259811e6fcecae8e82a5d1ec47
purplegummy <prasit.dhungyel@stu.jefferson.kyschools.us>
2022-08-29 17:11:59 -0400

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import components from './components' and add them here
+import { Navbar } from '../components/navigation/Navbar'
+export const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <p>Home</p>      
+    </div>
+  )
+}
