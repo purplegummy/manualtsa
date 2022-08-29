a1a31685488985ac6bef85b8e1de3485f291a3c2
purplegummy <prasit.dhungyel@stu.jefferson.kyschools.us>
2022-08-29 18:56:25 -0400

    added page not found page routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Route, Routes} from 'react-router-dom';
 import {Home} from './pages/Home';
 import {About} from './pages/About';
+import {NotFound} from './pages/NotFound';
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path='/*' element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export const NotFound = () => {
+  return (
+    <div>
+        <h1>Page Not Found</h1>
+        <Link to="/">Visit Our Homepage</Link>
+    </div>
+  )
+}
