21fc8a37289d332613d3c7ac63429f93c0ae9db0
purplegummy <prasit.dhungyel@stu.jefferson.kyschools.us>
2022-09-03 14:12:23 -0400

    navbar layout styles

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex flex-row h-20 w-full bg-neutral-200 justify-between p-6">
+      <div className="">Logo</div>
+      <div className="flex flex-row gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/">About</Link>
+        <Link to="/">Competitions</Link>
+        <Link to="/">Awards</Link>
+        <Link to="/">Contact</Link>
+      </div>
+
+    </div>
   )
 }
