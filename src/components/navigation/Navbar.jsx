0655f34020700bbee90da49032646e3f6ab4c058
purplegummy <prasit.dhungyel@stu.jefferson.kyschools.us>
2022-09-14 10:34:16 -0400

    updated fonts and added some styles for the navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 export const Navbar = () => {
   return (
-    <div className="flex flex-row h-20 w-full bg-neutral-200 justify-between p-6">
+    
+    <div className="sticky top-0 transition-all text-slate-600 font-poppins flex flex-row h-28 w-full items-center justify-between px-20">
       <div className="">Logo</div>
-      <div className="flex flex-row gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/">About</Link>
-        <Link to="/">Competitions</Link>
-        <Link to="/">Awards</Link>
-        <Link to="/">Contact</Link>
-      </div>
+      <div className="flex flex-row justify-between gap-16">
+        <div className="text-lg items-center flex flex-row gap-16">
+          <Link className="" to="/">Home</Link>
+          <Link to="/">About <FontAwesomeIcon className="text-red-500" icon={faAngleDown} /></Link>
+          <Link to="/">Awards</Link>
+          <Link to="/">Competitions</Link>
+       
+          
+          
 
+        </div>
+        <Link className="rounded-[15px] block h-ful py-2 px-8 bg-[#F12D4C] border-solid border-red-200 border-4 text-white" to="/">Contact</Link>
+      </div>
+      
     </div>
   )
 }
