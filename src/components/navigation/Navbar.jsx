3f05c4674c966ec8438c8a59a389daaebc62db04
purplegummy <prasit.dhungyel@stu.jefferson.kyschools.us>
2023-03-16 12:32:06 -0400

    Working on Navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -2,25 +2,34 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import '../../styles/nav.css'
+
 export const Navbar = () => {
+  const [dropDown, setDropDown] = React.useState(false);
   return (
     
     <div className="sticky top-0 transition-all text-slate-600 font-poppins flex flex-row h-28 w-full items-center justify-between px-20">
+    
       <div className="">Logo</div>
       <div className="flex flex-row justify-between gap-16">
         <div className="text-lg items-center flex flex-row gap-16 flex-nowrap">
           <Link className="" to="/">Home</Link>
-          <Link className="flex flex-nowrap items-center gap-2" to="/">About <FontAwesomeIcon className="text-red-500" icon={faAngleDown} /></Link>
-          <Link to="/">Awards</Link>
+          <Link onMouseEnter={() => {setDropDown(true)}} onMouseLeave={() => {setDropDown(false)}} className="drop flex flex-nowrap items-center gap-2" to="/">About <FontAwesomeIcon className="text-red-500" icon={faAngleDown} /></Link>
+          {dropDown && 
+            <div>
+
+            </div>
+          }
+          <Link  to="/">Awards</Link>
+          
           <Link to="/">Competitions</Link>
-       
+          
           
           
 
         </div>
-        <Link className="rounded-[15px] block h-ful py-2 px-8 bg-[#F12D4C] border-solid border-red-200 border-4 text-white" to="/">Contact</Link>
+        <Link className="contact rounded-[15px] block h-ful py-2 px-8 bg-[#F12D4C] border-solid border-red-200 border-4 text-white" to="/">Contact</Link>
       </div>
-      
     </div>
   )
 }
